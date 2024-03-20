eb65b0512164299756275582d94681bf5081dc22
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-20 14:05:54 -0400

    chore(Setup) Bootstrapped tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h2 className="bg-pink-200 h-20 w-full">Let's build</h2>
+    </div>
+  );
+}
+
+export default App;
